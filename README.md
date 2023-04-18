e9d2692772149cc5c8780ba76d5117fedc1d5460
tahara <tahara@artisan-jp.com>
2023-04-18 17:05:19 +0900

    rc01

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # web2023-partial
+
+開発中サイトの動画組み込みページ向けの制御スクリプトです。
+
+## スクリプトの概要
+- すべての動画について、表示領域に応じて自動的に再生・停止を行います。
+- スポンジ硬度説明動画は、再生タイムフレーム同期を行います。
+
+## 組み込み方法
+
+### スポンジ硬度説明
+
+- `dist/asset` 以下の動画ファイルをコピーします。
+- 対象`video`タグに下記の属性を付与します。
+```
+muted loop preload data-artisan-sync-play
+```
+
+### エイムスタイル説明
+- `dist/asset` 以下の動画ファイルをコピーします。
+- 対象`video`タグに下記の属性を付与します。
+```
+muted loop preload data-artisan-autopause
+```
+- 該当Rowの下に下記のようなコピーライト表記を追記してください。デザインはお任せします。
+```
+<div>
+    &copy;
+    <a href="https://www.diabotical.com/" target="_blank" rel="noopener noreferrer">
+        Diabotical
+    </a>, courtesy and copyright of <a href="http://www.thegdstudio.com/" target="_blank" rel="noopener noreferrer">GD Studio</a>.
+</div>
+```
+
+## スクリプトのロード
+- `<script defer>` タグを用いて`body`内でスクリプトを読み込んでください。
+
+注意: 現時点ではdefer属性または明示的にDOM構築後に実行する必要があります。\
+ソースをそのままHTML上に記述する必要がある場合は、書き換えますのでお申し付けください。
+
+
+## ファイル構成
+
+- `dist/index.js` \
+スクリプト本体
+
+- `dist/assets/videos/*.webm` \
+動画ファイル
+
+- `dist/index.html` \
+実装例
+
+- `src/index.ts` \
+スクリプトソースファイル
+
+## ビルド
+
+1. yarn install
+2. yarn build
---
+++ src/index.ts
@@ -0,0 +1,130 @@
+/*
+ * Autopause & sync playback of elaticity video
+ */
+((window, document) => {
+    // Settings & Target Element
+    const TARGET_VIDEO_ATTRIBUTE = 'data-artisan-sync-play';
+    const INTERSECT_VERTICAL_MARGIN_PX = 150;
+    const INTERSECT_DETECTION_MARGIN = `${INTERSECT_VERTICAL_MARGIN_PX}px ${INTERSECT_VERTICAL_MARGIN_PX}px 0px 0px`;
+    const videos = document.querySelectorAll<HTMLVideoElement>(`video[${ TARGET_VIDEO_ATTRIBUTE }]`);
+
+    // Only allow playback when all 3 videos are ready.
+    let canPlay = false;
+    
+    // Playback is synced clock based to make it dead simple.
+    let syncTimerStartedAt: number;
+
+    // Utility function to detect if all videos are ready to play
+    const getIfAllVideosReady = () => {
+        let isReady: boolean = true;
+        for (let i = 0; i < videos.length; i++) {
+            const video = videos[i];
+            if (video.readyState !== video.HAVE_ENOUGH_DATA) isReady = false;
+        }
+        return isReady;
+    }
+
+    // Playback control
+    const play = (video: HTMLVideoElement) => {
+        if (!canPlay) return;
+        if (!syncTimerStartedAt) throw new Error('syncTimerStartedAt is undefined, something\'s wrong with the first playback process');
+        const duration = video.duration;
+        video.currentTime = ((Date.now() - syncTimerStartedAt) / 1000) % duration;
+        return video.play();
+    }
+    const pauseAndReset = (video: HTMLVideoElement) => {
+        video.pause();
+        video.currentTime = 0;
+    }
+
+    // Utility to detect videos on screen upon before intersection event.
+    const checkIfInViewportAndControlPlayback = (video: HTMLVideoElement) => {
+        const videoRect = video.getBoundingClientRect();
+        const triggerTop = videoRect.top - INTERSECT_VERTICAL_MARGIN_PX, triggerBottom = videoRect.bottom + INTERSECT_VERTICAL_MARGIN_PX;
+        const isTopInViewport = triggerTop <= (window.innerHeight || document.documentElement.clientHeight);
+        const isBottomInViewport = triggerBottom <= (window.innerHeight || document.documentElement.clientHeight);
+        if (isTopInViewport || isBottomInViewport) return play(video);
+        pauseAndReset(video);
+    }
+
+    // Attach listeners
+    for (let i = 0; i < videos.length; i++) {
+        const video = videos[i];
+        // Create Intersection Observer to detect on/off screen event
+        const callback: IntersectionObserverCallback = (entries, observer) => {
+            entries.forEach(entry => {
+                if (!canPlay) return;
+                if (entry.isIntersecting) return play( entry.target as HTMLVideoElement );
+                pauseAndReset( entry.target as HTMLVideoElement );
+            });
+        };
+        const observerOpts: IntersectionObserverInit = { 
+            root: null, // defaults to viewport 
+            rootMargin: INTERSECT_DETECTION_MARGIN, 
+            threshold: 0 
+        };
+        const observer = new IntersectionObserver( callback, observerOpts);
+        observer.observe( video );
+
+        // Listen to video ready state to sync playback
+        video.addEventListener('canplaythrough', (e) => {
+            if (!canPlay && getIfAllVideosReady())  {
+                // this is the first playback.
+                canPlay = true;
+                // Set sync clock start time
+                syncTimerStartedAt = Date.now();
+                // manually checking if videos are in viewport as Intersection Observer will not fire until first intersection event (fired by scroll)
+                videos.forEach(video => checkIfInViewportAndControlPlayback(video));
+            }
+        }, { once: true });
+    }
+
+})(window, document);
+
+/*
+ * Autopause offscreen for aiming style vieos
+ */
+((window, document) => {
+    // Settings & Target Element
+    const TARGET_VIDEO_ATTRIBUTE = 'data-artisan-autopause';
+    const INTERSECT_VERTICAL_MARGIN_PX = 150;
+    const INTERSECT_DETECTION_MARGIN = `${INTERSECT_VERTICAL_MARGIN_PX}px ${INTERSECT_VERTICAL_MARGIN_PX}px 0px 0px`;
+    const videos = document.querySelectorAll<HTMLVideoElement>(`video[${ TARGET_VIDEO_ATTRIBUTE }]`);
+
+    // Playback control
+    const play = (video: HTMLVideoElement) => {
+        return video.play();
+    }
+    const pause = (video: HTMLVideoElement) => {
+        video.pause();
+    }
+
+    // Utility to detect videos on screen upon before intersection event.
+    const checkIfInViewportAndControlPlayback = (video: HTMLVideoElement) => {
+        const videoRect = video.getBoundingClientRect();
+        const triggerTop = videoRect.top - INTERSECT_VERTICAL_MARGIN_PX, triggerBottom = videoRect.bottom + INTERSECT_VERTICAL_MARGIN_PX;
+        const isTopInViewport = triggerTop <= (window.innerHeight || document.documentElement.clientHeight);
+        const isBottomInViewport = triggerBottom <= (window.innerHeight || document.documentElement.clientHeight);
+        if (isTopInViewport || isBottomInViewport) return play(video);
+        pause(video);
+    }
+
+    // Attach listeners
+    for (let i = 0; i < videos.length; i++) {
+        const video = videos[i];
+        // Create Intersection Observer to detect on/off screen event
+        const callback: IntersectionObserverCallback = (entries, observer) => {
+            entries.forEach(entry => {                
+                if (entry.isIntersecting) return play( entry.target as HTMLVideoElement );
+                pause( entry.target as HTMLVideoElement );
+            });
+        };
+        const observerOpts: IntersectionObserverInit = { 
+            root: null, // defaults to viewport 
+            rootMargin: INTERSECT_DETECTION_MARGIN, 
+            threshold: 0 
+        };
+        const observer = new IntersectionObserver( callback, observerOpts );
+        observer.observe( video );
+    }
+})(window, document);
